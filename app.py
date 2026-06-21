94e6586631035cd39153c2b1341b7241ddb6f0a8
allalexkr <allalexkr@gmail.com>
2026-06-21 22:40:16 +0300

    Next commit

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
     import uvicorn
     # Запуск приложения с указанием порта
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app:app", host="127.0.0.1", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
